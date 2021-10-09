4de7c86856180602e914af06d18ab63628757fb8
rusyska55011 <ruslan.nikandrov2015@yandex.ru>
2021-10-09 19:00:29 +0300

    main: DoRequests class created

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from root import TorSession, Parser
 
 
+class DoRequests(TorSession):
+    session_requests = 0
+    parser = Parser
+
+    def __init__(self, tor_session):
+        super(DoRequests, self).__init__(tor_session)
+        self.session = self.receive_session()
+        self.start_tor()
+
+    def __del__(self):
+        self.close_tor()
